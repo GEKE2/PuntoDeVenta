6d0e05f30500d9eb1610f8bbb1bdf6bcdd1eb4e2
Gloria <gloriavvcortez@gmail.com>
2024-10-21 10:13:10 -0600

    Sin errores

--- src/main/java/com/goya/puntodeventa/controller/ClienteController.java
+++ src/main/java/com/goya/puntodeventa/controller/ClienteController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.goya.puntodeventa.dto.ClienteDTO;
 import com.goya.puntodeventa.service.ClienteService;
 
-
+@RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
      
